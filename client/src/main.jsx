fb64d195d786c2186d7fab381b75fb4237185244
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2024-09-29 12:12:47 +0530

    update routers folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './routes/index.jsx'
+import router from './routers/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
